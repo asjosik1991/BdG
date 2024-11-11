8d9fa8d1067ca3a9addfff595f4852566c90e2e7
Askar Iliasov <A.Iliasov@science.ru.nl>
2024-11-11 12:14:54 +0100

    Update H_calculations.py

--- h_lattices/H_calculations.py
+++ h_lattices/H_calculations.py
@@ -6,32 +6,32 @@ def main():
 
     p=8
     q=3
-    l=3
+    l=4
     t=1
-    V=2
-    T=0.02
+    V=1
+    T=0.1
     mu=0
     
     #hypersample=hbdg.HyperLattice(p,q,l,t) #Hyperbolic lattice
 
-    hypersample=hbdg.HyperLattice(p,q,l,t, loadfile="adjs for h_lattices/8_3_256sites.mtx") #Hyperbolic lattice
-    #hypersample=hbdg.Tree_graph(q,l,t) #Tree graph
-    
+    #hypersample=hbdg.HyperLattice(p,q,l,t, loadfile="adjs for h_lattices/8_3_256sites.mtx") #Hyperbolic lattice
+    hypersample=hbdg.Tree_graph(q,l,t) #Tree graph
+    print(np.round(hypersample.spectrum(),4))
     #one BdG cycle and plotting the resu;t
-    BdGhypersample=hbdg.HyperBdG(hypersample,V,T,mu, uniform=True)
-    BdGhypersample.plot_lattice_spectrum()
-    #BdGhypersample.BdG_cycle()
+    BdGhypersample=hbdg.HyperBdG(hypersample,V,T,mu)
+    # BdGhypersample.plot_lattice_spectrum()
+    BdGhypersample.BdG_cycle()
     # #print(BdGhypersample.Delta)
-    # BdGhypersample.field_plot(np.round(BdGhypersample.Delta,4))
+    BdGhypersample.field_plot(np.round(BdGhypersample.Delta,4))
     
     #plot phase diagram
-    V_array=[2]
-    T_array=np.linspace(0.01, 0.3, num=30)
-    mu_array=np.linspace(-3, 3, num=30)
+    # V_array=[2]
+    # T_array=np.linspace(0.01, 0.3, num=30)
+    # mu_array=np.linspace(-3, 3, num=30)
 
-    hbdg.calculate_hyperdiagram(hypersample,V_array,mu_array,T_array, uniform=True)
-    hdiagram=hbdg.load_hyperdiagram(hypersample)
-    hbdg.plot_diagram(hypersample, hdiagram)
+    # hbdg.calculate_hyperdiagram(hypersample,V_array,mu_array,T_array, uniform=True)
+    # hdiagram=hbdg.load_hyperdiagram(hypersample)
+    # hbdg.plot_diagram(hypersample, hdiagram)
     
 
 
